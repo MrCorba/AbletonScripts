55279048a4ca01b4c3bc4affc3a48683e8067099
MrCorba <tools@corba-audiotechniek.nl>
2018-05-01 15:08:35 +0200

    Cleanup AbletonTransport class

--- AbletonScripts/AbletonTransport.py
+++ AbletonScripts/AbletonTransport.py
@@ -7,19 +7,22 @@ class AbletonTransport(TransportComponent):
 
     def __init__(self):
         TransportComponent.__init__(self)
-        self.set_up_controls()
+        self.setup_controls()
 
-    def set_up_controls(self):
+    def setup_controls(self):
         self.set_play_button(momentary_cc_button(PLAY_BUTTON))
         self.set_stop_button(momentary_cc_button(STOP_BUTTON))
-        self.set_seek_forward_button(momentary_cc_button(FFWD_BUTTON))
-        self.set_seek_backward_button(momentary_cc_button(RWD_BUTTON))
         self.set_record_button(momentary_cc_button(REC_BUTTON))
         self.set_loop_button(momentary_cc_button(LOOP_BUTTON))
         self.set_metronome_button(toggle_cc_button(METRONOME))
         self.set_tap_tempo_button(toggle_cc_button(TAP_TEMPO))
+        self.setup_seek_buttons()
         self.setup_punch_buttons()
         self.setup_nudge_buttons()
+
+    def setup_seek_buttons(self):
+        self.set_seek_forward_button(momentary_cc_button(FFWD_BUTTON))
+        self.set_seek_backward_button(momentary_cc_button(RWD_BUTTON))
 
     def setup_punch_buttons(self):
         self.set_punch_in_button(momentary_cc_button(PUNCH_IN))
