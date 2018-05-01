2fc8623a3e970257e4fe79252ea9378385e81bf2
MrCorba <tools@corba-audiotechniek.nl>
2018-05-01 15:08:17 +0200

    Add other transport controls

--- AbletonScripts/AbletonTransport.py
+++ AbletonScripts/AbletonTransport.py
@@ -1,7 +1,7 @@
 from _Framework.TransportComponent import TransportComponent
 
 from Consts import *
-from Util import momentary_cc_button
+from Util import momentary_cc_button, toggle_cc_button
 
 class AbletonTransport(TransportComponent):
 
@@ -16,3 +16,15 @@ class AbletonTransport(TransportComponent):
         self.set_seek_backward_button(momentary_cc_button(RWD_BUTTON))
         self.set_record_button(momentary_cc_button(REC_BUTTON))
         self.set_loop_button(momentary_cc_button(LOOP_BUTTON))
+        self.set_metronome_button(toggle_cc_button(METRONOME))
+        self.set_tap_tempo_button(toggle_cc_button(TAP_TEMPO))
+        self.setup_punch_buttons()
+        self.setup_nudge_buttons()
+
+    def setup_punch_buttons(self):
+        self.set_punch_in_button(momentary_cc_button(PUNCH_IN))
+        self.set_punch_out_button(momentary_cc_button(PUNCH_OUT))
+
+    def setup_nudge_buttons(self):
+        self.set_nudge_up_button(momentary_cc_button(NUDGE_UP))
+        self.set_nudge_down_button(momentary_cc_button(NUDGE_DOWN))
--- AbletonScripts/Util.py
+++ AbletonScripts/Util.py
@@ -4,3 +4,6 @@ from Consts import *
 
 def momentary_cc_button(midi_id):
     return ButtonElement(IS_MOMENTARY, CC_TYPE, MIDI_CHANNEL, midi_id)
+
+def toggle_cc_button(midi_id):
+    return ButtonElement(IS_TOGGLE, CC_TYPE, MIDI_CHANNEL, midi_id)
